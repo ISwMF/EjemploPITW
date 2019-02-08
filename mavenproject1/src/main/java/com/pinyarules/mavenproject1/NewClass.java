3c076c2d370c1a62b894a9f2e9add6fc826229da
BOG-A209-E-008 <BOG-A209-E-008@RGH-LS6-08.UMD.LOCAL.EDU>
2019-02-07 21:25:51 -0500

    Principios de cómo añadir una libreria usando Maven (Framework de Java)

---
+++ mavenproject1/src/main/java/com/pinyarules/mavenproject1/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pinyarules.mavenproject1;
+
+import org.joda.time.LocalTime;
+
+/**
+ *
+ * @author BOG-A209-E-008
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        LocalTime x = new LocalTime();
+        System.out.println(x);
+    }
+    
+}
